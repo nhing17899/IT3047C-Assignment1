b09e9bc8f8b0af2c2b0177af74f856b265ccd1a5
Nhi Nguyen <nhing17899@gmail.com>
2024-01-14 17:20:25 -0500

    Add GetName and AddName

---
+++ Assignment1/Controllers/NameController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Assignment1.Models;
+
+namespace Assignment1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        [HttpGet("GetName/{name?}")]
+        public string GetName(string name = "Nhi")
+        {
+            return $"My name is: {name}.";
+        }
+
+        [HttpPost("AddName")]
+        public string AddName(Name name)
+        {   
+            return name.MyName;
+        }
+    }
+}
---
+++ Assignment1/Models/Name.cs
@@ -0,0 +1,7 @@
+﻿namespace Assignment1.Models
+{
+    public class Name
+    {
+        public string MyName { get; set; }
+    }
+}
